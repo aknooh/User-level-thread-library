f592a58ed8ab3b92b4bb7fdad517c36fd2bc3db4
aknooh <aknooh@ucdavis.edu>
2017-02-07 12:10:25 -0800

    Added idea code into uthread.c

--- libuthread/uthread.c
+++ libuthread/uthread.c
@@ -12,6 +12,19 @@
 #include "queue.h"
 #include "uthread.h"
 
+queue_t Queue;					// main Queue to hold threads 
+								// global so all functions can access it
+
+// thread function struct to store a function
+// and its argument in the queue.
+struct thread_func{
+	char uthread_func_name[64];
+	void *arg;
+	//char *argChar;				// if arg is char
+//	int argInt;					// if arg is int
+};
+
+
 struct uthread_tcb {
 	/* TODO Phase 2 */
 };
@@ -48,5 +61,19 @@ struct uthread_tcb *uthread_current(void)
 
 void uthread_start(uthread_func_t start, void *arg)
 {
+	int newStatus;
+	thread_func start_func;
+	strcpy(start_func.uthread_func_name, (char**) start);
+	start_func.arg = arg; 
+	Queue = queue_create(void);		// create a a Queue object
+	newStatus = queue_enqueue(Queue,start);
+
+// I added some new stuff
+while(Queue.length()>-1)
+{
+	uthread_yield();		// yield to other threads 
+}
+
+
 	/* TODO Phase 2 */
 }
